7a521791c40f72785ab54cdb6cc340bf7f26ba23
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-06-07 02:20:01 -0300

    Foi um parto por conta da CORD policy, mas acredito que consegui.

--- script/cadastroDeEventos.js
+++ script/cadastroDeEventos.js
@@ -5,43 +5,51 @@
 ****************************************************/
 
 /* IMPLEMENTAÇÃO DO MÉTODO POST */
-
-
-// EVENT LISTENNER
-let retorno;
-
-
-
-
+const baseURL = "https://soundgarden-api.vercel.app";
 
 //FUNCTION THAT GETS THE VALUES SUBMITER BY THE ADMIN
 function getElements(){
-    
-    console.log("comecei a executar a função");
     let nome = document.getElementById("nome").value;
     let atracoes = document.getElementById("atracoes").value;
     let descricao = document.getElementById("descricao").value;
     let data = document.getElementById("data").value;
     let lotacao = document.getElementById("lotacao").value;
+
+    //debug only
     console.log("peguei os valores");
-    retorno =
-    {
+    console.log({
         "name": nome,
         "poster": '',
         "attractions": [atracoes],
         "description": descricao,
         "scheduled": data,
         "number_tickets": lotacao
-    };
-    console.log(retorno);
+    });
+    //debug only
+
+    alert("Tem certeza que deseja enviar evento para o Banco de Dados?");//debug only
+    axios({//POST is blocked in the browser due to CORS policy °~°
+        method: "post",
+        url: `${baseURL}//posts`,
+        data:{
+            "name": nome,
+            "poster": '',
+            "attractions": [atracoes],
+            "description": descricao,
+            "scheduled": data,
+            "number_tickets": lotacao
+        }
+    })
+    .then(response => console.log(response))//logs to the console. Can be a success message
+    .catch(error => console.log(error));   
 }
 
-console.log("*********  OI ***********");
-
+//Event handler
 document.getElementById("postEvent").addEventListener("submit", (event)=>{
-    console.log("OK, PEGUEI O EVENTO");
     event.preventDefault();
     getElements();
 });
+
+//THIS ONE IS MOSTLY DONE.
 
 
