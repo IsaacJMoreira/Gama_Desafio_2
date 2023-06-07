9e5f4b2479a6542a24f6c0ce2698db14a89f4b7d
Bruno Oliveira <49489698+byoliveira@users.noreply.github.com>
2023-06-07 14:55:10 -0300

    Update listarEventos.js
    Segue as observações levantadas de implementação, lembrando que os seletores aponta para tag responsável por exibir as informações

--- script/listarEventos.js
+++ script/listarEventos.js
@@ -1,3 +1,14 @@
+// não há necessidade de usar axios no projeto, pois quando for atribuir os valores que retornam da API pode encontrar dificuldade, usa o fetch que fica mais fácil
+// manipular e criar a table com os dados
+
+// sugestão
+
+const body = document.querySelector('body');
+const tbody = document.querySelector('#tabela-eventos');
+const loading = document.querySelector('#loading');
+
+const BASE_URL = 'https://soundgarden-api.vercel.app';
+
 axios.defaults.headers.common['X-Auth-Token'] =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
@@ -11,6 +22,49 @@ window.onload = function() {
     loadEvents();
   };
 
+
+// Sugestão para sua função loadEvents()
+// criar um tryctach que se encarregara de fazer a req e tratar a exeption caso haja
+
+body.onload = async () => {
+    try {
+        const responseEvents = await fetch(`${BASE_URL}/events`, { method: "GET" });
+        const contentResponseEvents = await responseEvents.json();
+        loading.style.display = "none";
+
+        for (let i = 0; i < 6; i++) {
+            const finalDate = new Date(contentResponseEvents[i].scheduled);
+
+            tbody.insertAdjacentHTML("beforeend", `
+                <tr>
+                    <th scope="row">
+                        ${i + 1}
+                    </th>
+                    <td>
+                        ${finalDate.toLocaleDateString('en-GB')}
+                    </td>
+                    <td>
+                        ${contentResponseEvents[i].name}
+                    </td>
+                    <td>
+                        ${contentResponseEvents[i].attractions}
+                    </td>
+                    <td>
+                        <a href="bookings.html?id=${contentResponseEvents[i]._id}" class="btn btn-dark">
+                        Check bookings</a>
+                        <a href="edit-event.html?id=${contentResponseEvents[i]._id}" class="btn btn-secondary">Edit</a>
+                        <a href="delete-event.html?id=${contentResponseEvents[i]._id}" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            `);
+        };
+
+    } catch (error) {
+        console.log(error);
+        loading.style.display = "none";
+        alert('Error!!!');
+    };
+};
 
 
 //FUNCTION THAT UPDATES THE EVENTS TO THE DOM
