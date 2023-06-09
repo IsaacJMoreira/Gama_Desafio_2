f8278dd7ab7409216982a031234de3a064da725e
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-06-09 20:59:45 -0300

    cabei... quase.falta tirar os debugs

--- script/script.js
+++ script/script.js
@@ -52,8 +52,9 @@ function loadEvents(){
          //HERE, THE CARDS ARE ACTUALY GENERATED WITH THE REAL DATA
          let maxCards = (eventos.data.length > 3)? 3 : eventos.data.length;//mostra no máximo 3 cards no index
         for (let i = 0; i < maxCards; i++ ){
-            createCard(eventos.data[i]);
+            createCard(eventos.data[i], i);
        }
+       listarEventosFazerReserva()
     })//logs to the console. Can be a success message
     .catch(error => console.log(error)); 
    
@@ -61,16 +62,98 @@ function loadEvents(){
 
 
 //FUNCTION THAT CREATES THE CARDS
-function createCard(element){
+function createCard(element, i){
     document.getElementById('divEventos').insertAdjacentHTML("beforeEnd", `
     <article class="evento card p-5 m-3">
                     <h2>${element.name} - ${element.scheduled}</h2>
                     <h4>${element.attractions}</h4>
                     <p>${element.description}</p>
-                    <a href="$" class="btn btn-primary">reservar ingresso</a>
+                    <a idEvento = "${element._id}" id = "botao-reservar${i}" class="btn btn-primary">reservar ingresso</a>
                 </article>
     `);
 }
+
+/*******************************************************/
+const listarEventosFazerReserva = async() => {
+
+    await fetch("https://soundgarden-api.vercel.app/events")
+        .then(response => response.text())
+        .then((data) => JSON.parse(data))
+        .then(listaDeEventos => {
+
+            //Ordenando a lista de Eventos
+            listaDeEventos.sort((a, b) => {
+                return new Date(a.scheduled) - new Date(b.scheduled);
+            });
+            // console.log(listaDeEventos);
+
+
+            //Filtrando a lista de Eventos para retornar apenas que ainda estão por vir
+            const eventosFuturos = listaDeEventos.filter((data) => {
+                const agora = new Date();
+                return new Date(data.scheduled) > agora
+            });
+
+            //For para resumir os eventos e retornar apenas os 3 primeros da lista de eventos Futuros.
+            //E criá-los na Home com a função Criar Estrutura Evento 
+          
+            modalReservarIngresso()
+        })
+        .catch(error => console.log('error', error));
+
+    const formReservarIngresso = document.querySelector("#reserva");
+
+    document.getElementById("botao-reservar0")
+        .addEventListener("click", (event) => {
+            const botaoAlvo = event.target
+            const idevento = botaoAlvo.getAttribute("idevento")
+            document.querySelector("#id").value = idevento;
+        })
+
+    document.getElementById("botao-reservar1")
+        .addEventListener("click", (event) => {
+            const botaoAlvo = event.target
+            const idevento = botaoAlvo.getAttribute("idevento")
+            document.querySelector("#id").value = idevento;
+        })
+
+    document.getElementById("botao-reservar2")
+        .addEventListener("click", (event) => {
+            const botaoAlvo = event.target
+            const idevento = botaoAlvo.getAttribute("idevento")
+
+            document.querySelector("#id").value = idevento;
+        })
+
+    const botaoConfirmar = document.querySelector("#myModal > div > div > div.modal-footer > button.btn.btn-primary")
+    botaoConfirmar.addEventListener("click", (event) => {
+        event.preventDefault();
+        const corpoPost = {
+            owner_name: formReservarIngresso[2].value,
+            owner_email: formReservarIngresso[3].value,
+            number_tickets: formReservarIngresso[1].value,
+            event_id: formReservarIngresso[0].value
+        }
+
+      
+        fetch(`https://soundgarden-api.vercel.app/bookings`, {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(corpoPost),
+            })
+            .then(() => {
+                alert("Parabéns, sua reserva está concluída!");
+            })
+            .catch(error => console.log('error', error));
+    })
+}
+
+
+
+/*******************************************************/
 
 /* FUNCIONALIDADE EXTRA - BANNER ROTATIVO */
 // Como é extra, vou fazer fade in das imagens, ok?
